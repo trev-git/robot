7730ea56fd66c70d503eaae07365d4bb95a071b0
trev-git <htaedshararam@gmail.com>
2024-07-08 10:51:01 +0300

    finish 6-point calibration

--- ui/main_interface/main_interface.py
+++ ui/main_interface/main_interface.py
@@ -4,13 +4,16 @@ from .calibration.calibration_window import CalibrationWindow
 
 
 class MainInterface(QMainWindow):
-    def __init__(self):
+    def __init__(self, robot):
         super(MainInterface, self).__init__()
         self.ui = Ui_MainInterface()
         self.ui.setupUi(self)
-        self.calibration_window = CalibrationWindow('asd')
-        self.setWindowTitle('Робот')
+        self.setWindowTitle("Робот")
+
+        self.robot = robot
+
         self.ui.calibration_six_points.clicked.connect(self.calibration)
 
     def calibration(self):
+        self.calibration_window = CalibrationWindow(self.robot)
         self.calibration_window.show()
